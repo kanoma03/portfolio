b2c39b6efeb6369dbcb9481b50e494cb1272f238
kanoma03 <87607492+kanoma03@users.noreply.github.com>
2025-05-28 00:36:51 +0900

    Add files via upload

---
+++ js/top.js
@@ -0,0 +1,18 @@
+// スライダー　トップ
+// $('.maincontents').slick({
+//   arrows:true,
+//   dots:true,
+//   asNavFor:'.slider',
+// });
+$('.slider').slick({
+  // asNavFor:'.maincontents',
+  centerMode: true,
+  centerPadding: '10%',
+  focusOnSelect: true,
+  // slidesToShow:3,
+  // slidesToScroll:1,
+  dots:true,
+  centerMode: true,
+  autoplay:true,
+  autoplaySpeed:4000,
+});
